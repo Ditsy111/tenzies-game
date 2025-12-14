6ff2108104f8596307aeea5f853e0c9a5e2cc948
unknown <darshana.chougule2426@gmail.com>
2025-12-15 02:54:49 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,44 @@
+import "./App.css"
+import Die from "./Die"
+import { useState } from "react"
+import { nanoid } from "nanoid"
+
+export default function App() {
+    const[dice, setDice]=useState(generateAllDice())
+    function generateAllDice(){
+        const newDice=[];
+        for(let i=0; i<10; i++){
+            newDice.push({
+            value: Math.ceil(Math.random() * 6),
+            isHeld: false,
+            id: nanoid()
+        })
+        }
+        return newDice
+    }
+    function rollBack(){
+        setDice(generateAllDice())
+    }
+
+    function hold(id){
+        console.log(id)
+    }
+
+    const diceElements = dice.map(dieObject => 
+        <Die 
+        key={dieObject.key}
+        value={dieObject.value} 
+        isHeld={dieObject.isHeld}
+        id={dieObject.id}
+        hold={()=>{dieObject.hold}}
+        />)
+    return(
+        <main>
+            <div className="dice-container">
+                {diceElements}
+            </div>
+            <button className="roll-dice" onClick={rollBack}>Roll</button>
+        </main>
+
+    )
+}
